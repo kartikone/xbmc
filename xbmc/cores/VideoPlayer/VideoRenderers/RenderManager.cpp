0cfd1c49259f7c5c5e8b9d34e5d89faf70986746
cpm <162544069+cpm-code@users.noreply.github.com>
2025-04-06 09:37:56 +0000

    TEMP Update

--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
@@ -769,15 +769,17 @@ void CRenderManager::Render(bool clear, DWORD flags, DWORD alpha, bool gui)
 
         double refreshrate, clockspeed;
         int missedvblanks;
-        info.vsync = StringUtils::Format("VSyncOff: {:.1f} latency: {:.3f} vt:{:.3f}",
-                                         (m_clockSync.m_syncOffset / 1000),
-                                         (DVD_TIME_TO_MSEC(m_displayLatency) / 1000.0f),
-                                         (m_videoLatencyTweak / 1000.0));
+
+        info.vsync = StringUtils::Format("VSync Off:{:.1f}", (m_clockSync.m_syncOffset / 1000));
+
         if (m_dvdClock.GetClockInfo(missedvblanks, clockspeed, refreshrate))
-        {
           info.vsync += StringUtils::Format("VSync: refresh:{:.3f} missed:{} speed:{:.3f}%",
-                                            refreshrate, missedvblanks, clockspeed * 100);
-        }
+                                            refreshrate, missedvblanks, (clockspeed * 100));
+
+        info.latency = StringUtils::Format("Latency: vt:{:.3f} at:{:.3f} ut:{:.3f}",
+                                           (m_videoLatencyTweak / 1000.0),
+                                           (m_audioLatencyTweak / 1000.0),
+                                           (m_videoDelay / 1000.0));
 
         m_debugRenderer.SetInfo(info);
       }
@@ -1064,11 +1066,11 @@ bool CRenderManager::AddVideoPicture(const VideoPicture& picture, volatile std::
     }
   }
 
-
   SPresent& m = m_Queue[index];
   m.presentfield = displayField;
   m.presentmethod = presentmethod;
   m.pts = picture.pts;
+  m.duration = picture.iDuration;
   m_queued.push_back(index);
   m_free.pop_front();
 
@@ -1178,41 +1180,84 @@ int CRenderManager::WaitForBuffer(volatile std::atomic_bool& bStop,
   return m_queued.size() + m_discard.size();
 }
 
-void CRenderManager::PrepareNextRender()
+void inline CRenderManager::SetPresentSource()
 {
-  if (m_queued.empty())
+  if (m_presentstarted)
   {
-    logM(LOGERROR, "CRenderManager", "asked to prepare with nothing available");
-    m_presentstep = PRESENT_IDLE;
-    m_presentevent.notifyAll();
-    return;
+    if (m_discard.empty() || (m_discard.back() != m_presentsource))
+      m_discard.push_back(m_presentsource);
+  }
+  m_presentsource = m_queued.front();
+  m_presentpts = m_Queue[m_presentsource].pts;
+  m_presentframetime = m_Queue[m_presentsource].duration;
+}
+
+void inline CRenderManager::Wait(useconds_t uSeconds)
+{
+  struct timespec target, now;
+  clock_gettime(CLOCK_MONOTONIC, &now);
+
+  target.tv_sec = uSeconds / 1000000;
+  target.tv_nsec = (uSeconds % 1000000) * 1000;
+
+  target.tv_sec += now.tv_sec;
+  target.tv_nsec += now.tv_nsec;
+
+  if (target.tv_nsec >= 1000000000) {
+    target.tv_sec++;
+    target.tv_nsec -= 1000000000;
   }
 
+  clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &target, NULL);
+}
+
+bool inline CRenderManager::Paused(bool paused, double clock)
+{
+  static double previousClock = 0;
+
+  // for pause, check for frame advance
+  bool check = paused ? (clock == previousClock) : paused;
+
+  previousClock = clock;
+
+  return check;
+}
+
+void CRenderManager::PrepareNextRender()
+{
   if (!m_showVideo && !m_forceNext)
     return;
+
+  double renderPts = m_dvdClock.GetClock();
+
+  float speed = m_dataCacheCore.GetSpeed();
+  bool paused = Paused((speed == 0.0f), renderPts);
+
+  if (paused)
+    return;
+
+  bool playing = (speed == 1.0f);
 
   // Make sure the queued are sorted by pts and no duplicates.
   std::sort(m_queued.begin(), m_queued.end(), [this](int a, int b) { return m_Queue[a].pts < m_Queue[b].pts; });
   auto last = std::unique(m_queued.begin(), m_queued.end());
   m_queued.erase(last, m_queued.end());
 
-  double frametime = 1.0 /
-                     static_cast<double>(CServiceBroker::GetWinSystem()->GetGfxContext().GetFPS()) *
-                     DVD_TIME_BASE;
+  SetPresentSource(); // get next frame
 
-  m_displayLatency = 0;
-
-  double clockPts = m_dvdClock.GetClock();
-  double renderPts = clockPts + m_displayLatency;
-
-  int nextFrameIndex = m_queued.front();
-  double nextFramePts = m_Queue[nextFrameIndex].pts;
   if (m_dvdClock.GetClockSpeed() < 0)
-    nextFramePts = renderPts;
+  {
+    logM(LOGINFO, "CRenderManager", "negative clock speed detected!");
+    m_presentpts = renderPts;
+  }
+
+  // How far away are we from the clock (renderPts)
+  double diff = (renderPts - m_presentpts);
 
   if (m_clockSync.m_enabled)
   {
-    double err = fmod(renderPts - nextFramePts, frametime);
+    m_presentframetime = 1.0 / static_cast<double>(CServiceBroker::GetWinSystem()->GetGfxContext().GetFPS()) * DVD_TIME_BASE;
+    double err = fmod(diff, m_presentframetime);
     m_clockSync.m_error += err;
     m_clockSync.m_errCount ++;
     if (m_clockSync.m_errCount > 30)
@@ -1224,52 +1269,48 @@ void CRenderManager::PrepareNextRender()
 
       m_dvdClock.SetVsyncAdjust(-average);
     }
-    renderPts += frametime / 2 - m_clockSync.m_syncOffset;
+    renderPts += m_presentframetime / 2 - m_clockSync.m_syncOffset;
+    diff = (renderPts - m_presentpts);
   }
   else
   {
     m_dvdClock.SetVsyncAdjust(0);
   }
 
-  logComponentM(LOGDEBUG, LOGAVTIMING, "CRenderManager",
-                "clockPts: [{:.3f}] renderPts: [{:.3f}] nextFramePts: [{:.3f}] "
-                "-> diff: [{:.3f}] render: [{:d}] "
-                "forceNext: [{:d}[] queueSize: [{:d}] frametime: [{:.3f}]",
-                (clockPts / DVD_TIME_BASE), (renderPts / DVD_TIME_BASE), (nextFramePts / DVD_TIME_BASE),
-                ((renderPts - nextFramePts) / DVD_TIME_BASE), (renderPts >= nextFramePts),
-                m_forceNext, m_queued.size(), (frametime / DVD_TIME_BASE));
-
-  if ((renderPts >= nextFramePts) || m_forceNext)
-  {
-    // push back present source index before other lates to keep order
-    if (m_presentstarted) m_discard.push_back(m_presentsource);
-
-    double diff = (renderPts - nextFramePts);
-    while ((diff > 62000) && (m_queued.size() > 2))
+  if (diff > 0)
+    while ((diff > -1000) && (m_queued.size() > 2))
     {
-      // skip late frames (over 62ms) if possible; if the queue is almost empty, we don't skip
-      // even if we should to avoid emptying the queue too fast
-      int late = m_queued.front();
-      m_queued.pop_front();
-
-      m_discard.push_back(late);
-      if (m_dataCacheCore.GetSpeed() == 1.0f) m_QueueSkip++;
-
-      diff = (renderPts - m_Queue[m_queued.front()].pts);
+      if (playing) m_QueueSkip++;
+      m_queued.pop_front(); // skip this frame
+      SetPresentSource();   // get next frame
+      diff = (renderPts - m_presentpts);
     }
 
-    int idx = m_queued.front();
+  double delay = diff; // keep delay for logging.
 
-    m_lateframes = static_cast<int>(std::max(0.0, diff / frametime));
-    m_presentstep = PRESENT_FLIP;
-    m_presentsource = idx;
-    m_presentstarted = true;
-    m_queued.pop_front();
-    m_presentpts = m_Queue[m_presentsource].pts;
-    m_presentevent.notifyAll();
+  // Seek may push the diff to a large negative value, make sure it is sensible. TODO should be better protected elsewhere.
+  if ((diff < 0) && (diff > -1000000))
+  {
+    //usleep(-diff);
+    Wait(-diff);
+    renderPts = m_dvdClock.GetClock();
+    diff = (renderPts - m_presentpts);
   }
 
-  if (m_presentstarted) m_dataCacheCore.SetRenderPts(m_Queue[m_presentsource].pts);
+  m_lateframes = static_cast<int>(std::max(0.0, diff / m_presentframetime));
+  m_presentstep = PRESENT_FLIP;
+  m_presentstarted = true;
+  m_queued.pop_front();
+  m_presentevent.notifyAll();
+
+  m_dataCacheCore.SetRenderPts(m_presentpts);
+
+  logM(LOGINFO, "CRenderManager", "render: [{:.3f}] next: [{:02d}] [{:.3f}] "
+                                  "diff: [{:.3f}] delay: [{:.3f}] "
+                                  "queued: [{:02d}] frametime: [{:.3f}] skip: [{:02d}] force: [{:d}] speed: [{:.1f}]",
+                                  (renderPts / DVD_TIME_BASE), m_presentsource, (m_presentpts / DVD_TIME_BASE),
+                                  (diff / DVD_TIME_BASE), (delay / DVD_TIME_BASE),
+                                  m_queued.size(), (m_presentframetime / DVD_TIME_BASE), m_QueueSkip, m_forceNext, speed);
 
 }
 
@@ -1292,7 +1333,7 @@ bool CRenderManager::GetStats(int &lateframes, double &pts, int &queued, int &di
 {
   std::unique_lock<CCriticalSection> lock(m_presentlock);
   lateframes = m_lateframes / 10;
-  pts = m_presentpts - m_displayLatency;
+  pts = m_presentpts;
   queued = m_queued.size();
   discard  = m_discard.size();
   return true;
@@ -1301,7 +1342,7 @@ bool CRenderManager::GetStats(int &lateframes, double &pts, int &queued, int &di
 double CRenderManager::GetRenderPts()
 {
   std::unique_lock<CCriticalSection> lock(m_presentlock);
-  return (m_presentpts - m_displayLatency);
+  return m_presentpts;
 }
 
 double CRenderManager::GetFramePts()
--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.h
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.h
@@ -132,6 +132,9 @@ public:
 
   int GetVideoLatencyTweak() { return m_videoLatencyTweak; }
 
+  void SetAudioLatencyTweak(int tweak) { m_audioLatencyTweak = tweak; }
+  int GetAudioLatencyTweak() { return m_audioLatencyTweak; }
+
   void SetVideoSettings(const CVideoSettings& settings);
 
 protected:
@@ -140,6 +143,9 @@ protected:
   void PresentFields(bool clear, DWORD flags, DWORD alpha);
   void PresentBlend(bool clear, DWORD flags, DWORD alpha);
 
+  void SetPresentSource();
+  void Wait(useconds_t uSeconds);
+  bool Paused(bool paused, double clock);
   void PrepareNextRender();
   bool IsPresenting();
   bool IsGuiLayer();
@@ -192,11 +198,13 @@ protected:
   ERENDERSTATE m_renderState = STATE_UNCONFIGURED;
   CEvent m_stateEvent;
 
-  /// Display latency tweak value from AdvancedSettings for the current refresh rate and resolution, and audio
-  /// in milliseconds
+  // Display latency tweak from AdvancedSettings for the current refresh rate and resolution
   std::atomic_int m_videoLatencyTweak = 0;
-  /// Display latency updated in PrepareNextRender in DVD clock units, includes m_latencyTweak
-  double m_displayLatency = 0.0;
+
+  // Display latency tweak from AdvancedSettings for audio in milliseconds
+  std::atomic_int m_audioLatencyTweak = 0;
+
+  // User set latency
   std::atomic_int m_videoDelay = {};
 
   int m_QueueSize = 2;
@@ -205,6 +213,7 @@ protected:
   struct SPresent
   {
     double         pts;
+    double         duration;
     EFIELDSYNC     presentfield;
     EPRESENTMETHOD presentmethod;
   } m_Queue[NUM_BUFFERS]{};
@@ -229,6 +238,7 @@ protected:
   bool m_forceNext = false;
   bool m_presentstarted = false;
   int m_presentsource = 0;
+  double m_presentframetime = 0;
   XbmcThreads::ConditionVariable m_presentevent;
   CEvent m_flushEvent;
   CEvent m_initEvent;
